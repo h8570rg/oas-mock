dbbc15f008e6bc5497d32448f015e09a1ef61685
h8570rg <namao0627@gmail.com>
2023-07-24 13:44:14 +0900

    add allowed origin

--- middleware.js
+++ middleware.js
@@ -1,5 +1,9 @@
 module.exports = (req, res, next) => {
-  res.header("access-control-allow-origin", "https://localhost:8000");
+  const allowedOrigins = ["https:/localhost:8000", "http://localhost:9000"];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("access-control-allow-origin", origin);
+  }
   res.header("access-control-allow-credentials", "true");
   res.header("x-viron-authtypes-path", "/authentication");
   res.header("access-control-expose-headers", "x-viron-authtypes-path");
